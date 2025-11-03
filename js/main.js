cfe189e4e4a654ba46c77acc6dac31280abb45a4
Monica Coca <mcocapelaz@gmail.com>
2025-11-03 10:22:48 +0100

    css and index.html

---
+++ js/main.js
@@ -0,0 +1,32 @@
+'use strict';
+
+// SECCIÓN DE QUERY-SELECTOR
+// Éstos son los elementos que nos traemos de la página HTML y usamos en el código
+
+
+
+// SECCIÓN DE DATOS
+// Aquí van los arrays y las variables que contantan datos de la aplicación
+
+
+// SECCIÓN DE FUNCIONES
+// Estos son funciones:
+//   - con código auxiliar
+//   - con código que usaremos en los eventos
+//   - para pintar (render) en la página.
+
+
+
+// SECCIÓN DE EVENTOS
+// Estos son los eventos a los que reacciona la página
+// Los más comunes son: click (en botones, enlaces), input (en ídem) y submit (en form)
+
+
+
+// SECCIÓN DE ACCIONES AL CARGAR LA PÁGINA
+// Este código se ejecutará cuando se carga la página
+// Lo más común es:
+//   - Pedir datos al servidor
+//   - Pintar (render) elementos en la página
+
+console.log('Página y JS cargados!');
